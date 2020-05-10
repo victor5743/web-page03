a8e27a3ed02aa10380f887e7580e224aa26f4803
Victor <vcs5743@gmail.com>
2020-05-10 13:07:22 -0400

    AppGames component added
    AppGames component added in Content component

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -11,10 +11,28 @@
 			</p>
 		</div>
 
-		<!-- Cards -->
-		<div class="flex">
+		<!-- Cards Section -->
+		<div class="flex flex-wrap w-full mb-6">
 			<div class="w-1/2" v-for="(item, index) in array" :key="item.id">
+				<!-- Card component -->
 				<Card :array="array[index]" />
+			</div>
+		</div>
+
+		<!-- Border -->
+		<div class="border-b border-gray-500 mb-8 mx-2"></div>
+
+		<!-- New Apps and Games Section -->
+		<!-- Title -->
+		<div class="flex justify-between mx-2 mb-4">
+			<h1 class="font-normal text-lg">New Apps and Games We Love</h1>
+			<a class="text-blue-600" href="#">See all</a>
+		</div>
+
+		<!-- AppsGames component -->
+		<div class="flex flex-wrap w-full mb-4">
+			<div class="w-1/2" v-for="(item, index) in data" :key="item.id">
+				<AppsGames :data="data[index]" />
 			</div>
 		</div>
 	</div>
@@ -22,6 +40,7 @@
 
 <script>
 import Card from './Card';
+import AppsGames from './AppsGames';
 
 export default {
 	data() {
@@ -40,10 +59,37 @@ export default {
 					url: 'discover3.jpg',
 				},
 			],
+			data: [
+				{
+					title: 'OmniFocus 2',
+					subtitle: 'App',
+					img: 'icon_omnifocus.png',
+					msg: 'Buy',
+				},
+				{
+					title: 'Old Man',
+					subtitle: 'Game',
+					img: 'icon_daisydisk.png',
+					msg: 'Buy',
+				},
+				{
+					title: 'QuarkXPress 2018 Pro',
+					subtitle: 'App',
+					img: 'icon_quark.png',
+					msg: 'Buy',
+				},
+				{
+					title: 'Pixelmator',
+					subtitle: 'Game',
+					img: 'icon_pixelmator.png',
+					msg: 'Buy',
+				},
+			],
 		};
 	},
 	components: {
 		Card,
+		AppsGames,
 	},
 };
 </script>
