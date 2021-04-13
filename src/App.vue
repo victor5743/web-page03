239a5939d9148feb07ca5cde983130fe2fe7de3d
victor4357 <victor.castro@codingtel.com>
2021-04-13 14:50:01 -0400

    Add comment
    This is an example of a description
    
    This is an example of a footer

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,21 @@
 <template>
-	<div class="font-jost min-h-screen tracking-wider leading-normal flex" id="app">
-		<Sidebar />
-		<Content />
-	</div>
+  <div class="font-jost min-h-screen tracking-wider leading-normal flex" id="app">
+    <!-- Sidebar component -->
+    <Sidebar />
+    <Content />
+  </div>
 </template>
 
 <script>
-import Sidebar from './components/Sidebar.vue';
-import Content from './components/Content.vue';
+import Sidebar from "./components/Sidebar.vue";
+import Content from "./components/Content.vue";
 
 export default {
-	name: 'App',
-	components: {
-		Sidebar,
-		Content,
-	},
+  name: "App",
+  components: {
+    Sidebar,
+    Content,
+  },
 };
 </script>
 
